72b4e11a709728241eb0cb08314f50aa13d40097
Keshav Karthik <kkarthik808@student.fuhsd.org>
2026-04-10 17:34:40 -0700

    Finished Arcade Engine 1
    Finished all code in arcade engine 1, but there is an error with javafx since I forgot to change jdk 21 to jdk 1.8. I will try to fix it myself or ask Mr. Ferrante after break.

---
+++ src/engine/Actor.java
@@ -0,0 +1,46 @@
+package engine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.image.ImageView;
+
+public abstract class Actor extends ImageView {
+	private AnimationTimer myTimer;
+	private boolean isRunning;
+	public World getWorld() {
+		return (World)getParent();
+	}
+	public abstract void act(long now);
+	public void addedToWorld() {
+		
+	}
+	public double getHeight() {
+		return getBoundsInParent().getHeight();
+	}
+	public <A extends Actor>java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls) {
+		List<A> allActors = getWorld().getObjects(cls);
+		List<A> intersectingActors = new ArrayList<A>();
+		for(int i = 0; i < allActors.size(); i++) {
+			if(allActors.get(i).intersects(getBoundsInParent())) {
+				intersectingActors.add(allActors.get(i));
+			}
+		}
+		return intersectingActors;
+	}
+	public <A extends Actor>A getOneIntersectingObject(java.lang.Class<A> cls) {
+		List<A> list = getIntersectingObjects(cls);
+		if(list.size()==0) {
+			return null;
+		}
+		return list.get(0);
+	}
+	public double getWidth() {
+		return getBoundsInParent().getWidth();
+	}
+	public void move(double dx, double dy) {
+		setX(getX()+dx);
+		setY(getY()+dy);
+	}
+}
---
+++ src/engine/World.java
@@ -0,0 +1,137 @@
+package engine;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+
+public abstract class World extends Pane{
+	private AnimationTimer myTimer;
+	private boolean isRunning;
+	private Set<KeyCode> keysPressed;
+	private boolean isWidthSet;
+	private boolean isHeightSet;
+	
+	public World() {
+		isWidthSet = false;
+		isHeightSet = false;
+		keysPressed = new HashSet<KeyCode>();
+		isRunning=false;
+		MyWidthChangeListener w = new MyWidthChangeListener();
+		widthProperty().addListener(w);
+		MyHeightChangeListener h = new MyHeightChangeListener();
+		heightProperty().addListener(h);
+		MySceneChangeListener s = new MySceneChangeListener();
+		sceneProperty().addListener(s);
+		MyKeyPressedEventHandler ke = new MyKeyPressedEventHandler();
+		setOnKeyPressed(ke);
+		MyKeyReleasedEventHandler ker = new MyKeyReleasedEventHandler();
+		setOnKeyReleased(ker);
+		myTimer = new MyAnimationTimer();
+	}
+	
+	public abstract void act(long now);
+	public void add(Actor actor) {
+		getChildren().add(actor);
+	}
+	public <A extends Actor>java.util.List<A> getObjectsAt(double x, double y, java.lang.Class<A> cls) {
+		List<A> actors = getObjects(cls);
+		List<A> addedActors = new ArrayList<A>();
+		for(int i = 0; i < actors.size(); i++) {
+			A actor = actors.get(i);
+			if (actor.getBoundsInParent().contains(x, y)) {
+		        actors.add(actor);
+		    }
+		}
+		return actors;
+	}
+	public <A extends Actor>java.util.List<A> getObjects(java.lang.Class<A> cls) {
+		List<A> list = new ArrayList<A>();
+		for(int i = 0; i < getChildren().size(); i++) {
+			if(cls.isInstance(getChildren().get(i))) {
+				list.add((A)getChildren().get(i));
+			}
+		}
+		return list;
+	}
+	public boolean isKeyPressed(javafx.scene.input.KeyCode code) {
+		return false;
+	}
+	public boolean isStopped() {
+		return !isRunning;
+	}
+	public abstract void onDimensionsInitialized();
+	public void remove(Actor actor) {
+		getChildren().remove(actor);
+	}
+	public void start() {
+		myTimer.start();
+		isRunning = true;
+	}
+	public void stop() {
+		myTimer.stop();
+		isRunning = false;
+	}
+	private class MyWidthChangeListener implements ChangeListener<Number> {
+		@Override
+		public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			if(newValue.intValue()>0) {
+				isWidthSet=true;
+			}
+			if(isWidthSet&&isHeightSet) {
+				onDimensionsInitialized();
+			}
+		}
+	}
+	private class MyHeightChangeListener implements ChangeListener<Number> {
+		@Override
+		public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			if(newValue.intValue()>0) {
+				isHeightSet=true;
+			}
+			if(isWidthSet&&isHeightSet) {
+				onDimensionsInitialized();
+			}
+		}
+	}
+	private class MySceneChangeListener implements ChangeListener<Scene> {
+		@Override
+		public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+			if(newValue!=null) {
+				requestFocus();
+			}
+		}
+	}
+	private class MyKeyPressedEventHandler implements EventHandler<KeyEvent> {
+		@Override
+		public void handle(KeyEvent e) {
+			keysPressed.add(e.getCode());
+		}
+	}
+	private class MyKeyReleasedEventHandler implements EventHandler<KeyEvent> {
+		@Override
+		public void handle(KeyEvent e) {
+			keysPressed.remove(e.getCode());
+		}
+	}
+	private class MyAnimationTimer extends AnimationTimer {
+		@Override
+		public void handle(long now) {
+			act(now);
+			for (Actor n : getObjects(Actor.class)) {
+			    if (n.getWorld() != null) {
+			        n.act(now);
+			    }
+			}
+		}
+	}
+}
